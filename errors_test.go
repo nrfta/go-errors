4857f6b95d5c9cd609d6c422c9d2ebd4a958fe63
Gene Armstrong <garmstrong@underline.com>
2023-12-05 11:49:47 -0800

    fix: error type assertion with errors.As

--- errors_test.go
+++ errors_test.go
@@ -139,4 +139,11 @@ var _ = Describe("Errors", func() {
 
 		Expect(fmt.Sprintf("%+v", errors.StackTrace(err))).To(ContainSubstring("/errors_test.go:"))
 	})
+
+	Describe("#Code", func() {
+		It("should return a wrapped error code", func() {
+			err := errors.Join(fmt.Errorf("test"), errors.InvalidArgument.New("invalid argument"))
+			Expect(errors.Code(err)).To(Equal(errors.InvalidArgument))
+		})
+	})
 })
